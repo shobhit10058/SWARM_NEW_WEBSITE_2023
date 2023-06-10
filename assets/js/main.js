fd27716af2e24ea2fa4f7fa290243742b20d863c
anushka192001 <anushkasingh100@yahoo.com>
2023-06-10 10:10:32 +0530

    ....

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+  /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* ==========================================================================
+       countdown timer
+       ========================================================================== */
+      var days=10;
+      var years=10;
+      var months=11;
+      var msec=new Date().getTime();
+      var sec=(Math.round(msec/1000))-1685471400;
+      var d=document.getElementById('days');
+      var m=document.getElementById('months');
+      var y=document.getElementById('years');
+      setInterval(setTime, 1000);
+      function setTime() {
+        console.log('0k working');
+        sec++;
+        if(sec>86400) {
+          sec=0;
+          days++;
+          if((months==1 && ((years%4==0 && days==30) || (days==29))) || (days==31 && (months==3 || months==5 || months==8 || months==10)) || days==32) {
+            days=0,months++;
+            if(months==11 && days==32) years++,days=0,months=0;
+          }
+          d.innerHTML=days;
+          m.innerHTML=months;
+          y.innerHTML=years;
+        }
+      }
+
+    /* Auto Close Responsive Navbar on Click
+    ========================================================*/
+    function close_toggle() {
+        if ($(window).width() <= 768) {
+            $('.navbar-collapse a').on('click', function () {
+                $('.navbar-collapse').collapse('hide');
+            });
+        }
+        else {
+            $('.navbar .navbar-inverse a').off('click');
+        }
+    }
+    close_toggle();
+    $(window).resize(close_toggle);
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+
+    /* Nivo Lightbox 
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+      });
+
+    // one page navigation 
+    $('.navbar-nav').onePageNav({
+            currentClass: 'active'
+    }); 
+
+    /* Counter
+    ========================================================*/
+    $('.counterUp').counterUp({
+     delay: 10,
+     time: 1500
+    });
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+  });      
+
+}(jQuery));
